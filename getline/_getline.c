37fdf85da7498477efbc1dcaa55015506d19f9fa
yahdielo <yahdielaz@gmail.com>
2023-12-15 19:19:49 -0400

    new commit

--- getline/_getline.c
+++ getline/_getline.c
@@ -1,1 +1,14 @@
 #include "_getline.h"
+/**
+ * 
+ */
+char *_getline(const int fd)
+{
+    static char buff[READ_SIZE];
+
+    while (read(fd, buff, READ_SIZE) > 0)
+    {
+      
+    }
+    return buff;
+}
--- getline/_getline.h
+++ getline/_getline.h
@@ -4,7 +4,9 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <unistd.h>
 #define READ_SIZE 256
 
+char *_getline(const int fd);
 
 #endif /* _GETLINE_H */
