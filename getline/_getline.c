d6bb3a415564112f9c0210bb9c7b6f9141d93b3e
yahdielo <yahdielaz@gmail.com>
2023-12-16 14:32:29 -0400

    test

--- getline/_getline.c
+++ getline/_getline.c
@@ -8,33 +8,48 @@ char *_getline(const int fd)
     static size_t buffLen = 0;
     ssize_t bytesRead;
     char *line;
-    size_t i, newLinePos = 0;
-
+    size_t i, newLinePos = 0, restetBuff;
+    /* to initialize the buflen*/
     if(buffLen == 0)
     {
+        /*read a chuk of data drom file size of READ_zide to bytesRead*/
         bytesRead = read(fd, buff, READ_SIZE);
         if (bytesRead <= 0) {
             return NULL; 
         }
+        /*assing the lenght if the chunk od data read to bufflen*/
         buffLen = (size_t)bytesRead;
     }
+
     while (newLinePos < buffLen && buff[newLinePos] != '\n')
     {
         newLinePos++;
     }
-
-    line = malloc(buffLen + 1);
+    /*to correcly print the file content, i need to know where is the position of the nes line
+        and allocate memory for that line + 1 to include the null byte*/
+    line = malloc(newLinePos + 1);
     if (line == NULL) {
         perror("Error allocating memory");
         exit(EXIT_FAILURE);
     }
-
-    for (i = 0; i < buffLen; ++i) {
+    /*copy the content of the buffer to the dynamic allocated memory
+        the memory is allocated just unitl new line is found, to correctly print the file
+        line by line*/
+    for (i = 0; i < newLinePos; ++i) {
         line[i] = buff[i];
-
+        if(line[i] == EOF)
+            break;
     }
-    line[buffLen] = '\0';
-    buffLen = 0;
+    /*null terminate the line*/
+    buff[newLinePos] = '\0';
+    /*the reset boffer is needed to start the boffer back again from 0, to correctly start allocating the new line*/
+    restetBuff = buffLen - newLinePos - 1;
+    for( i = 0; i < newLinePos; ++i)
+    {
+        buff[i] = buff[newLinePos + 1 + i];
+    }
+
+    buffLen =  restetBuff;
 
     return line;
 }
