66cc6fc84766c39db6c625a6d7764c0c6a055426
yahdielo <yahdielaz@gmail.com>
2023-12-16 17:25:53 -0400

    old code, i want to see some things on the endge casses

--- getline/_getline.c
+++ getline/_getline.c
@@ -8,51 +8,29 @@ char *_getline(const int fd)
     static size_t buffLen = 0;
     ssize_t bytesRead;
     char *line;
-    size_t i, newLinePos = 0, restetBuff;
-    /* to initialize the buflen*/
+    size_t i;
+
     if(buffLen == 0)
     {
-        /*read a chuk of data drom file size of READ_zide to bytesRead*/
         bytesRead = read(fd, buff, READ_SIZE);
         if (bytesRead <= 0) {
             return NULL; 
         }
-        /*assing the lenght if the chunk od data read to bufflen*/
         buffLen = (size_t)bytesRead;
     }
-
-    while (newLinePos < buffLen && buff[newLinePos] != '\n')
-    {
-        newLinePos++;
-    }
-    /*to correcly print the file content, i need to know where is the position of the nes line
-        and allocate memory for that line + 1 to include the null byte*/
-    line = malloc(newLinePos + 1);
+    line = malloc(buffLen + 1);
     if (line == NULL) {
         perror("Error allocating memory");
         exit(EXIT_FAILURE);
     }
-    /*copy the content of the buffer to the dynamic allocated memory
-        the memory is allocated just unitl new line is found, to correctly print the file
-        line by line*/
-    for (i = 0; i < newLinePos; ++i) {
+
+    for (i = 0; i < buffLen; ++i) {
         line[i] = buff[i];
-        if(line[i] == EOF)
+        if (line[i] == EOF)
             break;
     }
-    /*null terminate the line*/
-    buff[newLinePos] = '\0';
-    /* Ones the chunk of data from  the buffer before the new line is allocated,
-        and we still have some data in the buffer, we need to reset the buffer to start from 0,
-        to correctly allocate the new line, in a new line when printing it.*/
-    restetBuff = buffLen - newLinePos - 1;
-    line = realloc(line, restetBuff + 1);
-    for( i = 0; i < newLinePos; ++i)
-    {
-        line[i] = buff[newLinePos + 1 + i];
-    }
-
-    buffLen =  restetBuff;
+    line[buffLen] = '\0';
+    buffLen = 0;
 
     return line;
 }
