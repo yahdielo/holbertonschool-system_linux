08fdcdf006516426203c51877fad2af4adf961dd
yahdielo <yahdielaz@gmail.com>
2023-12-15 22:47:13 -0400

    get line

--- getline/_getline.c
+++ getline/_getline.c
@@ -5,10 +5,30 @@
 char *_getline(const int fd)
 {
     static char buff[READ_SIZE];
+    static size_t buffLen = 0;
+    ssize_t bytesRead;
+    char *line;
+    size_t i
 
-    while (read(fd, buff, READ_SIZE) > 0)
+    if(buffLen == 0)
     {
-      
+        bytesRead = read(fd, buff, READ_SIZE);
+        if (bytesRead <= 0) {
+            return NULL; 
+        }
+        buffLen = (size_t)bytesRead;
     }
-    return buff;
+    line = malloc(buffLen + 1);
+    if (line == NULL) {
+        perror("Error allocating memory");
+        exit(EXIT_FAILURE);
+    }
+
+    for (i = 0; i < buffLen; ++i) {
+        line[i] = buff[i];
+    }
+    line[buffLen] = '\0';
+    buffLen = 0;
+
+    return line;
 }
